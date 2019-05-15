16796976e121b3f9a86c97b0e23e9c27baf8a4b3
Kevin M <kevin.mahone19@gmail.com>
2019-05-15 11:07:18 -0400

    auth fixes

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -27,7 +27,52 @@ function signUp(event) {
       url: "/api/user/register",
       method: "POST",
       data: userData
-    }).then(function (accessToken) {
+    }).then(function (userData) {
+      console.log(userData);
+      return swal({
+        title: userData.message,
+        icon: "success"
+      })
+    })
+    .then(function () {
+      $("#signup").tab("hide");
+      $("#login").tab("show");
+
+    })
+    .catch(err => {
+      console.log(err);
+      return swal({
+        title: err.responseJSON.error,
+        icon: "error"
+      });
+    });
+}
+
+function login(event) {
+  event.preventDefault();
+
+  const userData = {
+    email: $("#email-input-login")
+      .val()
+      .trim(),
+    password: $("#password-input-login")
+      .val()
+      .trim()
+  };
+
+  if (!userData.email || !userData.password) {
+    return swal({
+      title: "Looks like you're missing something, please try again!",
+      icon: "error"
+    });
+  }
+
+  $.ajax({
+      url: "/api/user/login",
+      method: "POST",
+      data: userData
+    })
+    .then(function (accessToken) {
       console.log(accessToken);
       localStorage.setItem("accessToken", accessToken);
       getUserProfile();
@@ -38,6 +83,28 @@ function signUp(event) {
         title: err.responseJSON.error,
         icon: "error"
       });
+    });
+}
+
+function getUserProfile() {
+  const token = localStorage.getItem("accessToken");
+
+  $.ajax({
+      url: "/api/user",
+      method: "GET",
+      headers: {
+        authorization: `Bearer ${token}`
+      }
+    })
+    .then(function (userData) {
+      console.log(userData);
+      $("#user-tabs, #forms, #right-column-title").hide();
+      $("#user-info").show();
+      $("#full-name").text(userData.fullName);
+    })
+    .catch(err => {
+      console.log(err);
+      handleError(err.responseJSON);
     });
 }
 
@@ -77,7 +144,7 @@ function handleError(errorData) {
     icon: "warning"
   }).then(() => {
     $("#user-info").hide();
-    $("#user-tabs, #forms, $#right-column-title").show();
+    $("#user-tabs, #forms, #right-column-title").show();
     $("#login.tab").tab("show");
   });
 }
@@ -94,8 +161,10 @@ $(document).ready(function () {
   }
 });
 
-$("#location").on("submit", function(){
+$("#location").on("submit", function (event) {
+  event.preventDefault();
   var $btn = $("<button>")
     .text($("#location"))
     .attr("data-location", $("#location"))
+    .apendTo("#results");
 })
